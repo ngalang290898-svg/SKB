e050c70d5e1603251e0241a9ecf1001db314c1f4
ngalang290898-svg <ngalang290898@gmail.com>
2025-11-02 01:34:15 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,4 @@
 import './globals.css'
-import { Inter, Poppins } from 'next/font/google'
-import { LanguageProvider } from './contexts/LanguageContext'
-
-const inter = Inter({ 
-  subsets: ['latin'],
-  variable: '--font-body'
-})
-
-const poppins = Poppins({
-  weight: ['400', '500', '600', '700'],
-  subsets: ['latin'],
-  variable: '--font-heading'
-})
 
 export const metadata = {
   title: 'SK Bebuloh WP Labuan - Official Website',
@@ -24,11 +11,9 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className={`${inter.variable} ${poppins.variable}`}>
-      <body className="font-body">
-        <LanguageProvider>
-          {children}
-        </LanguageProvider>
+    <html lang="en">
+      <body>
+        {children}
       </body>
     </html>
   )
