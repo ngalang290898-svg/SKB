716661788a019b52c5d97044b0d0e832025983f8
ngalang290898-svg <ngalang290898@gmail.com>
2025-11-02 01:47:10 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,8 +4,10 @@ const nextConfig = {
     appDir: true,
   },
   images: {
-    domains: ['your-supabase-project.supabase.co'],
+    domains: ['uyfsrvxneioimldkubvo.supabase.co'],
   },
+  // Allow longer builds for initial setup
+  staticPageGenerationTimeout: 1000,
 }
 
 module.exports = nextConfig
