f7f0a5d89daeddffe418ef82f857f732bc6a6aee
ngalang290898-svg <ngalang290898@gmail.com>
2025-11-02 16:00:44 +0800

    Update NewsSection.tsx

--- app/components/home/NewsSection.tsx
+++ app/components/home/NewsSection.tsx
@@ -1,6 +1,8 @@
 import { getPosts } from '../../../lib/fetchers';
 import NewsSectionClient from './NewsSectionClient';
 
+export const dynamic = 'force-dynamic';
+
 export default async function NewsSection() {
   const posts = await getPosts();
   return <NewsSectionClient posts={posts} />;
