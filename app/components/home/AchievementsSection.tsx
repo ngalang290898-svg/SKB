ffe0334cc19855acf1db3d62e171250e83ccef53
ngalang290898-svg <ngalang290898@gmail.com>
2025-11-02 17:19:24 +0800

    Update AchievementsSection.tsx

--- app/components/home/AchievementsSection.tsx
+++ app/components/home/AchievementsSection.tsx
@@ -1,6 +1,7 @@
 import { getAchievements } from '../../../lib/fetchers';
 import AchievementsClient from './AchievementsClient';
 
+// Prevent long static build timeouts
 export const dynamic = 'force-dynamic';
 
 export default async function AchievementsSection() {
@@ -8,9 +9,9 @@ export default async function AchievementsSection() {
 
   const stats = {
     total_students:
-      achievements.reduce((s, i) => s + (i.student_count || 0), 0) || 850,
+      achievements.reduce((sum, item) => sum + (item.student_count || 0), 0) || 850,
     total_awards:
-      achievements.reduce((s, i) => s + (i.award_count || 0), 0) || 120,
+      achievements.reduce((sum, item) => sum + (item.award_count || 0), 0) || 120,
     total_activities: achievements.length || 45,
     alumni_count: 1250,
   };
