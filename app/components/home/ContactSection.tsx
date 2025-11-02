5b736c24f58a3e45c59fc38395a3bd27af9a1bf8
ngalang290898-svg <ngalang290898@gmail.com>
2025-11-02 16:01:37 +0800

    Update ContactSection.tsx

--- app/components/home/ContactSection.tsx
+++ app/components/home/ContactSection.tsx
@@ -1,6 +1,8 @@
 import { getSchoolMetadata } from '../../../lib/fetchers';
 import ContactClient from './ContactClient';
 
+export const dynamic = 'force-dynamic';
+
 export default async function ContactSection() {
   const school = await getSchoolMetadata();
   return <ContactClient school={school} />;
