7c1b88406659af1b4bd479373488b071c700ec33
ngalang290898-svg <ngalang290898@gmail.com>
2025-11-02 17:20:56 +0800

    Update StaffHighlight.tsx

--- app/components/home/StaffHighlight.tsx
+++ app/components/home/StaffHighlight.tsx
@@ -1,6 +1,7 @@
 import { getFeaturedStaff } from '../../../lib/fetchers';
 import StaffHighlightClient from './StaffHighlightClient';
 
+// Prevent long static build timeouts
 export const dynamic = 'force-dynamic';
 
 export default async function StaffHighlight() {
