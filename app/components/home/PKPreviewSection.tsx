700fcde0ed57be2eb41f6adf7fd5e7a0009156f0
ngalang290898-svg <ngalang290898@gmail.com>
2025-11-02 15:59:43 +0800

    Update PKPreviewSection.tsx

--- app/components/home/PKPreviewSection.tsx
+++ app/components/home/PKPreviewSection.tsx
@@ -1,6 +1,8 @@
 import { getPKSections } from '../../../lib/fetchers';
 import PKPreviewClient from './PKPreviewClient';
 
+export const dynamic = 'force-dynamic';
+
 export default async function PKPreviewSection() {
   const pkSections = await getPKSections();   // 👈 corrected name
   return <PKPreviewClient pkSections={pkSections} />;
