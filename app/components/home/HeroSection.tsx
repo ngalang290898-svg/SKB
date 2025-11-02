1638c9f4dfb0a3816b299fa6a92e4de17feaed8a
ngalang290898-svg <ngalang290898@gmail.com>
2025-11-02 16:01:55 +0800

    Update HeroSection.tsx

--- app/components/home/HeroSection.tsx
+++ app/components/home/HeroSection.tsx
@@ -1,5 +1,7 @@
 import HeroClient from './HeroClient';
 
+export const dynamic = 'force-dynamic';
+
 export default function HeroSection() {
   // Static content only – just return client-side visual
   return <HeroClient />;
